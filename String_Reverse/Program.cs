9254e90dadb5e4efc4411b363f3cc54374bae009
Eddie Garcia <eddieg031@gmail.com>
2021-04-08 22:32:56 -0500

    Palindrome

---
+++ String_Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace String_Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Word, changeword;
+            Word = "Race car";
+            Word = Word.ToLower().Replace(" ", "");
+            char[] changedWord = Word.ToCharArray();
+            Array.Reverse(changedWord);
+            changeword = new string(changedWord);
+            bool b = Word.Equals(changeword, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine($"{Word} is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{Word} is not a Palindrome");
+            }
+        }
+    }
+}
